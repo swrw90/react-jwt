c62fbce660108158e42ff56f50e2254220961947
Seth Watson <seth.watson1@gmail.com>
2018-03-28 10:12:09 -0700

    added some error handling

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+
+//all requests filter through morgan and a log is returned in the console
 const morgan = require('morgan');
 app.use(morgan('dev'));
 
@@ -12,5 +14,23 @@ app.use(morgan('dev'));
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
 
+
+//returns error for requests not found by other routes and forwards the error
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+
+//returns error for failed requests or errors coming from anywhere
+app.use((error, req, res, next) => {
+    error.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
