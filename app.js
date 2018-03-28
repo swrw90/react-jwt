e9a80638d6ef3f2ab9bbec38f764e1ac23926a09
Seth Watson <seth.watson1@gmail.com>
2018-03-28 09:58:04 -0700

    installed morgan package

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+
 //only requests starting with /*example* are handled by 2nd argument. 
 //requests starting with /*example* are forwarded to the route file defined by 2nd argument.
 app.use('/products', productRoutes)
