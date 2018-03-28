beec33ea345b17cb1cf678e8d17911357c3285c4
Seth Watson <seth.watson1@gmail.com>
2018-03-28 09:29:02 -0700

    added product routes and CRUD functionality, imported productRoutes to server side app file

---
+++ api/routes/products.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+
+//handles GET requests to /products 
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Hanlding GET requests to /products'
+    });
+});
+
+//handles POST requests to /products 
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Hanlding POST requests to /products',
+        id: id
+    });
+});
+
+//handles specific product via productId variable, assigns it to new const id via params
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'Special product id received'
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+//handles updating a specific product via id
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Product updated'
+    });
+});
+
+//handles deleting a specific product via id
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product'
+    });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
+const productRoutes = require('./api/routes/products');
 
-//confirms response was received via res status 200
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'Success'
-    });
-})
+//only requests starting with /products are handled by 2nd argument. 
+//requests starting with /products are forwarded to productRoutes file.
+app.use('/products', productRoutes)
 
 module.exports = app;
