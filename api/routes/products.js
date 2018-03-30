989b6cd786df7756dd1e43df6481da4dd44f1a33
Seth Watson <seth.watson1@gmail.com>
2018-03-30 16:02:24 -0700

    added request object to delete response

--- api/routes/products.js
+++ api/routes/products.js
@@ -130,7 +130,14 @@ router.delete('/:productId', (req, res, next) => {
     Product.remove({ _id: id })
         .exec()
         .then(result => {
-            res.status(200).json({ result })
+            res.status(200).json({
+                message: 'Product deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http//localhost:5000/products/',
+                    body: { name: String, price: Number }
+                }
+            })
         })
         .catch(err => {
             console.log(err);
