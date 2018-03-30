27b35a2f46f59151657df99b99262c4f950fa67a
Seth Watson <seth.watson1@gmail.com>
2018-03-30 15:48:20 -0700

    added require to productSchema, added details to res object in product post and get

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,11 +8,25 @@ const Product = require('../models/product');
 //handles GET requests to /products 
 router.get('/', (req, res, next) => {
     Product.find()
+        .select('name price _id')
         .exec()
         .then(docs => {
-            console.log(docs);
+            const response = {
+                count: docs.length,
+                products: docs.map(doc => {
+                    return {
+                        name: doc.name,
+                        price: doc.price,
+                        _id: doc._id,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:5000/products/' + doc._id
+                        }
+                    }
+                })
+            };
             if (docs.length >= 0) {
-                res.status(200).json(docs);
+                res.status(200).json(response);
             } else {
                 res.status(404).json({
                     message: 'No entries found'
@@ -37,8 +51,16 @@ router.post('/', (req, res, next) => {
     product.save().then(result => {
         console.log(result);
         res.status(201).json({
-            message: 'Hanlding POST requests to /products',
-            createdProduct: product
+            message: 'Created product successfully',
+            createdProduct: {
+                name: result.name,
+                price: result.price,
+                _id: result._id,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:5000/products/' + result._id
+                }
+            }
         });
     })
         .catch(err => {
