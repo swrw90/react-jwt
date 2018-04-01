1c70a5571288c21ad6285aa2405d9a0afdacead8
Seth Watson <seth.watson1@gmail.com>
2018-03-31 17:42:14 -0700

    added fileFilter based on jpeg || png passed to upload limits configuration

--- api/routes/products.js
+++ api/routes/products.js
@@ -15,10 +15,23 @@ const storage = multer.diskStorage({
     }
 });
 
+const fileFilter = (req, file, cb) => {
+    //reject a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
 
-const upload = multer({ storage: storage, limits: {
-    fileSize: 1024 * 1024 * 5
-}});
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
 
 const Product = require('../models/product');
 
