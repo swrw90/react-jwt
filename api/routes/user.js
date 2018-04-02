e16d401a4e4bc7701c46f4392833afa14ae649e1
Seth Watson <seth.watson1@gmail.com>
2018-04-01 23:36:27 -0700

    Added conditional to determine account exists already

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,34 +6,45 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 router.post('/signup', (req, res, next) => {
-    bcrypt.hash(req.body.password, 10, () => {
-        if (err) {
-            return res.status(500).jsong({
-                error: err
-            })
-        } else {
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash
-            });
-            user
-                .save()
-                .then(result => {
-                    console.log(result)
-                    res.status(201).json({
-                        message: 'User created'
-                    });
-                })
-                .catch(err => {
-                    console.log(err);
-                    res.status(500).json({
-                        error: err
-                    })
-                })
-        }
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(422).json({
+                    message: 'Mail exists'
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, () => {
+                    if (err) {
+                        return res.status(500).jsong({
+                            error: err
+                        })
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user
+                            .save()
+                            .then(result => {
+                                console.log(result)
+                                res.status(201).json({
+                                    message: 'User created'
+                                });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                })
+                            })
+                    }
+                });
+            }
+        })
+        .catch()
 
-    });
 });
 
 module.exports = router;
