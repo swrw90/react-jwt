0ce621d39038b27d0859fce4c5f51f661e26ab4a
Seth Watson <seth.watson1@gmail.com>
2018-04-01 23:12:33 -0700

    added bcrypt and salt to userjs

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
 
@@ -8,7 +9,13 @@ router.post('/signup', (req, res, next) => {
     const user = new User({
         _id: new mongoose.Types.ObjectId(),
         email: req.body.email,
-        password: req.body.password
+        password: bcrypt.hash(req.body.email, 10, () => {
+            if (err) {
+                return res.status(500).jsong({
+                    error: err
+                })
+            }
+        })
     });
 });
 
