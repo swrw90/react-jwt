c27bff3673dd7f7103cff84cdfd8b245c375d7c9
Seth Watson <seth.watson1@gmail.com>
2018-04-04 15:11:40 -0700

    moved products post func from routes to controllers

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -1,4 +1,6 @@
 const Product = require('../models/product');
+const mongoose = require('mongoose');
+
 
 exports.products_get_all = (req, res, next) => {
     Product.find()
@@ -31,6 +33,37 @@ exports.products_get_all = (req, res, next) => {
         .catch(err => {
             console.log(err);
             res.status(500).json({
+                error: err
+            });
+        });
+}
+
+exports.products_create_product = checkAuth, upload.single('productImage'), (req, res, next) => {
+    console.log(req.file);
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price,
+        productImage: req.file.path
+    });
+    product.save().then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: 'Created product successfully',
+            createdProduct: {
+                name: result.name,
+                price: result.price,
+                _id: result._id,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:5000/products/' + result._id
+                }
+            }
+        });
+    })
+        .catch(err => {
+            console.log(err);
+            res.stats(500).json({
                 error: err
             });
         });
--- api/routes/products.js
+++ api/routes/products.js
@@ -43,36 +43,7 @@ const Product = require('../models/product');
 router.get('/', ProductsController.products_get_all);
 
 //handles POST requests to /products, returns created product
-router.post('/', checkAuth, upload.single('productImage'), (req, res, next) => {
-    console.log(req.file);
-    const product = new Product({
-        _id: new mongoose.Types.ObjectId(),
-        name: req.body.name,
-        price: req.body.price,
-        productImage: req.file.path
-    });
-    product.save().then(result => {
-        console.log(result);
-        res.status(201).json({
-            message: 'Created product successfully',
-            createdProduct: {
-                name: result.name,
-                price: result.price,
-                _id: result._id,
-                request: {
-                    type: 'GET',
-                    url: 'http://localhost:5000/products/' + result._id
-                }
-            }
-        });
-    })
-        .catch(err => {
-            console.log(err);
-            res.stats(500).json({
-                error: err
-            });
-        });
-});
+router.post('/', checkAuth, ProductsController.products_create_product);
 
 //handles specific product via productId variable, assigns it to new const id via params
 router.get('/:productId', (req, res, next) => {
