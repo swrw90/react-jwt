2b66226b1e32b2982584515e23ec5c7b2f8454f6
Seth Watson <seth.watson1@gmail.com>
2018-04-04 15:03:29 -0700

    exported all orders routes functions to orders controllers

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -9,97 +9,15 @@ const checkAuth = require('../auth/check-auth');
 const OrdersController = require('../controllers/orders');
 
 //handles GET requests to /orders 
-router.get('/', OrdersController.orders_get_all);
-
+router.get('/', checkAuth, OrdersController.orders_get_all);
 
 //handles POST requests to /orders, returns created order
-router.post('/', checkAuth, (req, res, next) => {
-    Product.findById(req.body.productId)
-        .then(product => {
-            if (!product) {
-                return res.status(404).json({
-                    message: 'Product not found'
-                });
-            }
-            const order = new Order({
-                _id: mongoose.Types.ObjectId(),
-                quantity: req.body.quantity,
-                product: req.body.productId
-            });
-            return order.save();
-        })
-        .then(result => {
-            console.log(result);
-            res.status(201).json({
-                message: 'Order stored',
-                createdOrder: {
-                    _id: result._id,
-                    quantity: result.quantity
-                },
-                request: {
-                    type: 'GET',
-                    url: 'http://localhost:5000/orders/' + result._id
-                }
-            })
-                .catch(err => {
-                    console.log(err);
-                    res.status(500).json({
-                        message: 'Product not found',
-                        error: err
-                    });
-                });
-        })
-});
-
+router.post('/', checkAuth, OrdersController.orders_create_order);
 
 //handles GET requests for a specific order via orderId
-router.get('/:orderId', checkAuth, (req, res, next) => {
-    Order.findById(req.params.orderId)
-        .populate('product')
-        .exec()
-        .then(order => {
-            if (!order) {
-                return res.status(404).json({
-                    message: 'Order not found'
-                })
-            }
-            res.status(200).json({
-                order: order,
-                request: {
-                    type: 'GET',
-                    url: 'http://localhost:5000/orders'
-                }
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: err
-            });
-        });
-});
-
+router.get('/:orderId', checkAuth, OrdersController.orders_get_order);
 
 //handles DELETE requests for a specific order via orderId
-router.delete('/:orderId', checkAuth, (req, res, next) => {
-    Order.remove({ _id: req.params.orderId })
-        .exec()
-        .then(result => {
-            res.status(200).json({
-                message: 'Order deleted',
-                request: {
-                    type: 'POST',
-                    url: 'http://localhost:5000/orders',
-                    body: { product: 'ID', quantity: 'Number' }
-                }
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: err
-            });
-        })
-});
-
-
+router.delete('/:orderId', checkAuth, OrdersController.orders_delete_order);
 
 module.exports = router;
