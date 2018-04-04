f3dd7eae653045c155764fb3a10b56ad24074ffd
Seth Watson <seth.watson1@gmail.com>
2018-04-04 15:30:00 -0700

    moved user login func from routes to controllers

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -46,3 +46,54 @@ exports.user_signup = (req, res, next) => {
         })
 };
 
+
+//Checks user authenticated to login
+exports.user_login = (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            // Checks if user count is less than one. If so fail.
+            if (user.length < 1) {
+                return res.status(401).json({
+                    message: 'Authorization Failed.'
+                });
+            }
+            console.log(user[0].password);
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                // Check for error
+                if (err) {
+                    return res.status(401).json({
+                        message: 'Authorization Failed.'
+                    });
+                }
+
+
+                if (result) {
+                    const token = jwt.sign(
+                        {
+                            email: user[0].email,
+                            userId: user[0]._id
+                        },
+                        process.env.JWT_KEY,
+                        {
+                            expiresIn: '1h'
+                        }
+                    ); console.log(token);
+                    return res.status(200).json({
+                        message: 'Authorization successful',
+                        token: token
+                    });
+                }
+
+                return res.status(401).json({
+                    message: 'Authorization Failed.'
+                });
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+};
--- api/routes/user.js
+++ api/routes/user.js
@@ -6,58 +6,13 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 const UserController = require('../controllers/user');
-router.post('/signup', );
 
 
-router.post('/login', (req, res, next) => {
-    User.find({ email: req.body.email })
-        .exec()
-        .then(user => {
-            // Checks if user count is less than one. If so fail.
-            if (user.length < 1) {
-                return res.status(401).json({
-                    message: 'Authorization Failed.'
-                });
-            }
-            console.log(user[0].password);
-            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-                // Check for error
-                if (err) {
-                    return res.status(401).json({
-                        message: 'Authorization Failed.'
-                    });
-                }
+//Creates a new user
+router.post('/signup', UserController.user_signup);
 
-                
-                if (result) {
-                    const token = jwt.sign(
-                        {
-                            email: user[0].email,
-                            userId: user[0]._id
-                        },
-                        process.env.JWT_KEY,
-                        {
-                            expiresIn: '1h'
-                        }
-                    ); console.log(token);
-                    return res.status(200).json({
-                        message: 'Authorization successful',
-                        token: token
-                    });
-                }
 
-                return res.status(401).json({
-                    message: 'Authorization Failed.'
-                });
-            })
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
-});
+router.post('/login', UserController.user_login);
 
 
 router.delete('/:userId', (req, res, next) => {
