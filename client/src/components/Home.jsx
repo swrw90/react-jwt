a6766e719bdfff4d81dd5e12e49ca2572c94a2cc
Seth Watson <seth.watson1@gmail.com>
2018-04-07 14:19:15 -0700

    styled and positioned Home content

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import './Home.css';
 
 class Home extends Component {
     render() {
@@ -9,10 +10,27 @@ class Home extends Component {
                 <Jumbotron>
                     <h2>Some Header Text</h2>
                     <p>Additional text for filler etc etc...</p>
+                    <Link to="/about">
+                        <Button bsStyle="success">About</Button>
+                    </Link>
                 </Jumbotron>
-                <Link to="/about">
-                    <Button bsStyle="success">About</Button>
-                </Link>
+                <Row className="show-grid text-center">
+                    <Col xs={12} sm={4} className="person-wrapper">
+                        <Image src="assets/sethw02.jpg" className="profile-picture" />
+                        <h3>Test1</h3>
+                        <p>Ridiculus mus pellentesque, maecenas hac praesent ante habitasse scelerisque tristique neque. Luctus conubia cum accumsan risus sociosqu, aliquet tempor dis.</p>
+                    </Col>
+                    <Col xs={12} sm={4} className="person-wrapper">
+                        <Image src="assets/sethw02.jpg" className="profile-picture" />
+                        <h3>Test1</h3>
+                        <p>Ridiculus mus pellentesque, maecenas hac praesent ante habitasse scelerisque tristique neque. Luctus conubia cum accumsan risus sociosqu, aliquet tempor dis.</p>
+                    </Col>
+                    <Col xs={12} sm={4} className="person-wrapper">
+                        <Image src="assets/sethw02.jpg" className="profile-picture" />
+                        <h3>Test1</h3>
+                        <p>Ridiculus mus pellentesque, maecenas hac praesent ante habitasse scelerisque tristique neque. Luctus conubia cum accumsan risus sociosqu, aliquet tempor dis.</p>
+                    </Col>
+                </Row>
             </Grid>
         );
     }
