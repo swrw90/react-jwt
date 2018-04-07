362a3ab133817b0424b6ab631de2e821f79d413f
Seth Watson <seth.watson1@gmail.com>
2018-04-06 21:12:52 -0700

    started navbar component

---
+++ client/src/components/landing-page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render() {
+        return(
+            <div>
+            <div className="navbar">
+                <ul>
+                <li> Sign Up </li>
+                <li> Log In </li>
+                </ul>
+            </div>
+            </div>
+        )
+    }
+}
