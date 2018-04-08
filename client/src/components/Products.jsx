e3aef5faf860ff268745fa23aaac7fc45a3964b6
Seth Watson <seth.watson1@gmail.com>
2018-04-08 16:31:07 -0700

    some styling on products

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -18,18 +18,18 @@ class Products extends Component {
                         <Col xs={12} sm={4} bsClass="clearfix" className="person-wrapper">
                             <Image src="assets/night-product-watch-dramatic-84475.jpeg" circle className="profile-picture" />
                             <h3>Classy Watch<label className="price">$500</label></h3>
-                            <p>Ridiculus mus pellentesque, maecenas hac praesent ante habitasse scelerisque tristique neque. Luctus conubia cum accumsan risus sociosqu, aliquet tempor dis.</p>
+                            <p className="product-description">Ridiculus mus pellentesque, maecenas hac praesent ante habitasse scelerisque tristique neque. Luctus conubia cum accumsan risus sociosqu, aliquet tempor dis.</p>
                         </Col>
                         <Col xs={12} sm={4} bsClass="clearfix" className="person-wrapper">
                             <Image src="assets/pexels-photo-947885.jpeg" circle className="profile-picture" />
                             <h3>Cool Shades <label className="price">$20</label></h3>
-                            <p>Ridiculus mus pellentesque, maecenas hac praesent ante habitasse scelerisque tristique neque. Luctus conubia cum accumsan risus sociosqu, aliquet tempor dis.</p>
+                            <p className="product-description">Ridiculus mus pellentesque, maecenas hac praesent ante habitasse scelerisque tristique neque. Luctus conubia cum accumsan risus sociosqu, aliquet tempor dis.</p>
                         </Col>
                         <Col xs={12} sm={4} bsClass="clearfix" className="person-wrapper">
                             <Image src="assets/pexels-photo-322674.jpeg" circle className="profile-picture" />
                             <h3>Stylish Bracelets<label className="price">$10</label></h3>
                             <div xs={12} sm={4} lg={12}>
-                                <p>Ridiculus mus pellentesque, maecenas hac praesent ante habitasse scelerisque tristique neque. Luctus conubia cum accumsan risus sociosqu, aliquet tempor dis.</p>
+                                <p className="product-description">Ridiculus mus pellentesque, maecenas hac praesent ante habitasse scelerisque tristique neque. Luctus conubia cum accumsan risus sociosqu, aliquet tempor dis.</p>
                             </div>
                         </Col>
                     </Row>
@@ -37,10 +37,11 @@ class Products extends Component {
                         <Row>
                             <Col xs={6} md={4}>
                                 <Thumbnail src="assets/pexels-photo-290043.jpeg" alt="242x200">
-                                    <h3>Bay-Ran</h3>
+                                    <h3>Bay-Ran </h3>
                                     <p>A brief description about these super cool glasses. Wow, you should buy these!</p>
                                     <p>
                                         <Button bsSize="xs" bsStyle="success">Add Item</Button>&nbsp;
+                                        <label className="price">$100</label>
                                     </p>
                                 </Thumbnail>
                             </Col>
@@ -50,15 +51,17 @@ class Products extends Component {
                                     <p>A brief description about these super cool glasses. Wow, you should buy these!</p>
                                     <p>
                                         <Button bsSize="xs" bsStyle="success">Add Item</Button>&nbsp;
+                                        <label className="price">$120</label>
                                     </p>
                                 </Thumbnail>
                             </Col>
                             <Col xs={6} md={4}>
                                 <Thumbnail src="assets/glasses-fluke-angel-therapy-glasses-glass-golden-53156.jpeg" alt="242x200">
-                                    <h3>Parby Warker</h3>
-                                    <p>A brief description about these super cool glasses. Wow, you should buy these!</p>
+                                    <h3>Parby Warker </h3>
+                                    <p >A brief description about these super cool glasses. Wow, you should buy these!</p>
                                     <p>
                                         <Button bsSize="xs" bsStyle="success">Add Item</Button>&nbsp;
+                                        <label className="price">$140</label>
                                     </p>
                                 </Thumbnail>
                             </Col>
