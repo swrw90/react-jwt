f543eab9a3d9244f5a352a82b848dafe35e42ba8
Seth Watson <seth.watson1@gmail.com>
2018-04-07 21:14:07 -0700

    added about css and img

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import './About.css';
 
 class About extends Component {
     render() {
         return (
             <div>
-            About Component
+                <Image src="assets/dog-people" className="header-image" />
+                <Grid>
+                    <Col xs={12} sm={8} smOffset={2}>
+                        <Image src="assets/sethw02.jpg" className="about-profile-picture" rounded />
+                        <h3>Something Anything</h3>
+                        <p>Ridiculus mus pellentesque, maecenas hac praesent ante habitasse scelerisque tristique neque. Luctus conubia cum accumsan risus sociosqu, aliquet tempor dis.</p>
+                        <p>Ridiculus mus pellentesque, maecenas hac praesent ante habitasse scelerisque tristique neque. Luctus conubia cum accumsan risus sociosqu, aliquet tempor dis.</p>
+                        <p>Ridiculus mus pellentesque, maecenas hac praesent ante habitasse scelerisque tristique neque. Luctus conubia cum accumsan risus sociosqu, aliquet tempor dis.</p>
+                        <p>Ridiculus mus pellentesque, maecenas hac praesent ante habitasse scelerisque tristique neque. Luctus conubia cum accumsan risus sociosqu, aliquet tempor dis.</p>
+
+                    </Col>
+                </Grid>
             </div>
         );
     }
