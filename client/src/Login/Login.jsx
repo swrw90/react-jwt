41b29ff38808eca285c68bebf9c859441d5a60b8
Seth Watson <seth.watson1@gmail.com>
2018-04-08 22:58:25 -0700

    added props to form, added mapStateToProps

--- client/src/Login/Login.jsx
+++ client/src/Login/Login.jsx
@@ -2,20 +2,21 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Jumbotron, Grid, Row, Col, ControlLabel, Button, Checkbox, Form, FormGroup, FormControl } from 'react-bootstrap';
 import './Login.css';
+import { connect } from 'react-redux';
 
 
 class Login extends Component {
     render() {
         return (
             <div>
-                <Grid>
-                    <Form horizontal>
+                <Grid className="login-form-wrapper">
+                    <Form horizontal name="loginForm">
                         <FormGroup controlId="formHorizontalEmail">
                             <Col componentClass={ControlLabel} sm={2}>
                                 Email
                             </Col>
                             <Col sm={10}>
-                                <FormControl type="email" placeholder="Email" />
+                                <FormControl type="email" name="email" placeholder="Email" />
                             </Col>
                         </FormGroup>
                         <FormGroup controlId="formHorizontalPassword">
@@ -23,7 +24,7 @@ class Login extends Component {
                                 Password
                              </Col>
                             <Col sm={10}>
-                                <FormControl type="password" placeholder="Password" />
+                                <FormControl type="password" name="password" placeholder="Password" />
                             </Col>
                         </FormGroup>
                         <FormGroup>
@@ -33,13 +34,21 @@ class Login extends Component {
                         </FormGroup>
                         <FormGroup>
                             <Col smOffset={2} sm={10}>
-                                <Button type="submit">Sign in</Button>
+                                <Button type="submit" value="Login">Sign in</Button>
                             </Col>
                         </FormGroup>
                     </Form>
                 </Grid>
             </div>
         );
+    }
+}
+
+mapStateToProps = (state) => {
+    return {
+        isLoginPending: state.isLoginPending,
+        isLoginSuccess: state.isLoginSuccess,
+        loginError: state.loginError
     }
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import createRedux from 'redux';
 import { Provider } from 'react-redux';
+import store from './redux/reducers/store';
 
 ReactDOM.render(
-    // <Provider store={store}>
-    <App />,
-    // </Provider>,
+    <Provider store={store}>
+        <App />,
+     </Provider>,
     document.getElementById('root'));
 registerServiceWorker();
