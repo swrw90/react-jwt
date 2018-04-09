91f5164d709e0e10a05570a54e577754613d6b8f
Seth Watson <seth.watson1@gmail.com>
2018-04-08 19:30:28 -0700

    positioned text

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -13,33 +13,30 @@ class Login extends Component {
                         <FormGroup controlId="formHorizontalEmail">
                             <Col componentClass={ControlLabel} sm={2}>
                                 Email
-    </Col>
+                            </Col>
                             <Col sm={10}>
                                 <FormControl type="email" placeholder="Email" />
                             </Col>
                         </FormGroup>
-
                         <FormGroup controlId="formHorizontalPassword">
                             <Col componentClass={ControlLabel} sm={2}>
                                 Password
-    </Col>
+                             </Col>
                             <Col sm={10}>
                                 <FormControl type="password" placeholder="Password" />
                             </Col>
                         </FormGroup>
-
                         <FormGroup>
                             <Col smOffset={2} sm={10}>
                                 <Checkbox>Remember me</Checkbox>
                             </Col>
                         </FormGroup>
-
                         <FormGroup>
                             <Col smOffset={2} sm={10}>
                                 <Button type="submit">Sign in</Button>
                             </Col>
                         </FormGroup>
-                    </Form>;
+                    </Form>
                 </Grid>
             </div>
         );
