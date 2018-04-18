b8880ef6d7d48b345ba5143d6a3925f889c55649
Seth Watson <seth.watson1@gmail.com>
2018-04-18 15:41:33 -0700

    added styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import { Well, Jumbotron, Grid, Row, Col, Image, Button, ButtonGroup } from 'react-bootstrap';
 import './Home.css';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <Image src="assets/adult-casual-clothes-296881.jpg" className="header-image" />
                 <Grid>
+                    <Jumbotron className="home-banner">
+                        <h1>Welcome to Clothing Co.</h1>
+                        <p className="home-banner-text">Summer is here and we have the best look for you... </p>
+                    </Jumbotron>
                     <Row>
                         <Col xs={12} sm={8} smOffset={2}>
-                            <Jumbotron > <h2>Checkout our summertime deals!</h2>
-                                <Link to="/products">
-                                    <Button bsStyle="success" bsSize="small">Learn More</Button>
-                                </Link>
-                            </Jumbotron>
                             <Image src="assets/pexels-photo-322207.jpeg" className="about-profile-picture" rounded />
                             <h4>Quality in every thread</h4>
                             <p>Ridiculus mus pellentesque, maecenas hac praesent ante habitasse scelerisque tristique neque. Luctus conubia cum accumsan risus sociosqu, aliquet tempor dis.</p>
-                            <p>Ridiculus mus pellentesque, maecenas hac praesent ante habitasse scelerisque tristique neque. Luctus conubia cum accumsan risus sociosqu, aliquet tempor dis.</p>
+                            <p>Ridiculus mus pellentesque, maecenas hac praesent ante habitasse scelerisque tristique neque.</p>
+                            <hr />
+                            <Row >
+                                <Col justified >
+                                    <Jumbotron > <h2>Checkout our summertime deals!</h2>
+                                    </Jumbotron>
+                                </Col>
+                            </Row>
+                            <div className="container-fluid"><ButtonGroup justified>
+                                <Button bsSize="small" href="/login">Sign Up</Button>
+                                <Button bsSize="small" href="/about">Learn More</Button>
+                                <Button bsSize="small" href="/products">Shop Now</Button>
+                            </ButtonGroup></div>
+                            <hr />
                         </Col>
                     </Row>
                 </Grid>
