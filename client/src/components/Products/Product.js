7b44cd5ed06513c5f0c2dd5115541e80d4aef3ea
Seth Watson <seth.watson1@gmail.com>
2018-04-23 12:27:29 -0700

    adding item to cartItems array via handleClick

--- client/src/components/Products/Product.js
+++ client/src/components/Products/Product.js
@@ -1,38 +1,50 @@
 import React from 'react';
 import './Products.css';
-import { Col, Button, Thumbnail, Image } from 'react-bootstrap';
+import { Col, Button, Thumbnail, Image, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { LoadingState} from '../../redux/reducers/product.reducers';
 const rootUrl = 'http://localhost:5000/';
 
 
 class Product extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            productCounter: 0,
+            cartItems: [],
+        }
 
-    handleSubmit = (e) => {
-        let count = 0;
-        e.preventDefault(e);
-        console.log(this.props + 'got called');
-        this.props.add(this.state);
-        this.setState({
-            productCounter: count += count++ ,
-            cartItem: this.props.product._id.push()
-        });
+        this.handleClick = this.handleClick.bind(this);
     }
 
+    handleClick = (e) => {
+        let cartItem = this.props.product;
+        let cartItems = [];
+        e.preventDefault(e);
+        let count = 0;
+        console.log(this.props);
+        this.setState({
+            productCounter: count++,
+            cartItems: cartItems.push(cartItem),
+        });
+    }
     render() {
         return (
             <div>
                 <Col xs={6} md={4}>
-                    <Thumbnail src={rootUrl + this.props.product.productImage} alt="242x200" className="product-image" >
-                        <h3>
-                            <Link target="_blank" className="product-header" to={rootUrl + this.props.product.productImage} href={rootUrl + this.props.product.productImage}>{this.props.product.name}
-                            </Link>
-                        </h3>
-                        <p>{this.props.product.description}</p>
-                        <p>
-                            <Button bsSize="xs" bsStyle="success" name="add" onSubmit={this.props.handleSubmit}>Add Item</Button>&nbsp;
+                    <Form>
+                        <Thumbnail src={rootUrl + this.props.product.productImage} alt="242x200" className="product-image" >
+                            <h3>
+                                <Link target="_blank" className="product-header" to={rootUrl + this.props.product.productImage} href={rootUrl + this.props.product.productImage}>{this.props.product.name}
+                                </Link>
+                            </h3>
+                            <p>{this.props.product.description}</p>
+                            <p>
+                                <Button bsSize="xs" bsStyle="success" name="add"  onClick={this.handleClick}>Add Item</Button>&nbsp;
                                         <label className="price">${this.props.product.price}</label>
-                        </p>
-                    </Thumbnail>
+                            </p>
+                        </Thumbnail>
+                    </Form>
                 </Col>
             </div>
         )
