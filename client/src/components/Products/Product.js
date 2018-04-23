f53bd936c449dd808abe01b03b402298305ac937
Seth Watson <seth.watson1@gmail.com>
2018-04-23 15:00:00 -0700

    working on add and addItem

--- client/src/components/Products/Product.js
+++ client/src/components/Products/Product.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './Products.css';
 import { Col, Button, Thumbnail, Image, Form, Badge } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { LoadingState} from '../../redux/reducers/product.reducers';
+import { add, addItem } from '../../redux/reducers/product.reducers';
+import { connect } from 'react-redux';
 const rootUrl = 'http://localhost:5000/';
 
 
@@ -10,18 +11,18 @@ const rootUrl = 'http://localhost:5000/';
 class Product extends React.Component {
 
     handleClick = (e) => {
-        let cartItem = this.props;
-        let cartItems = [];
         e.preventDefault(e);
-        let count = 0;
+        let cartItem = this.props
+        let cartItems = []
+        let count = 0
         this.setState({
             productCounter: count++,
             cartItems: cartItems.push(cartItem),
         });
-        console.log(cartItems)
-        console.log(count)
-        
+        add(cartItem);
     }
+
+
     render() {
         return (
 
@@ -35,7 +36,7 @@ class Product extends React.Component {
                             </h3>
                             <p>{this.props.product.description}</p>
                             <p>
-                                <Button bsSize="xs" bsStyle="success" name="add"  onClick={this.handleClick}>Add Item</Button>&nbsp;
+                                <Button bsSize="xs" bsStyle="success" name="add" onClick={this.handleClick}>Add Item</Button>&nbsp;
                                         <label className="price">${this.props.product.price}</label>
                             </p>
                         </Thumbnail>
@@ -46,5 +47,18 @@ class Product extends React.Component {
     }
 }
 
-export default Product;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addToCart: (cartItem) => dispatch(addItem(cartItem)),
+    };
+};
+
+const mapStateToProps = (state) => {
+    return {
+        products: state.productCounter,
+        cartItems: state.cartItems
+    }
+}
+
+export default connect(mapDispatchToProps, mapStateToProps)(Product);
 
