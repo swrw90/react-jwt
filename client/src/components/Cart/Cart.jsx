bbd244d9d2b1e3c5611e6fa83ee5f850ec18d9b0
Seth Watson <seth.watson1@gmail.com>
2018-04-26 16:22:31 -0700

    checkout form only displays if the cart has items

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -10,10 +10,18 @@ class Cart extends React.Component {
     displayCartItem() {
         let cart = this.props.cart;
         var cartItemComponent = cart.map(function (cart) {
-            return  <CartItem key={cart._id} cartItem={cart} />
-            
+            return <CartItem key={cart._id} cartItem={cart} />
+
         });
         return cartItemComponent;
+    }
+
+    displayCheckoutForm() {
+        let cart = this.props.cart;
+        var checkoutFormComponent = cart.map(function (cart) {
+            return <CheckoutForm key={cart._id} cartItem={cart} />
+        });
+        return checkoutFormComponent;
     }
 
     render() {
@@ -29,7 +37,8 @@ class Cart extends React.Component {
                     <Col xs={12} md={8}>
                         {this.displayCartItem()}
                     </Col>
-                    <CheckoutForm/>
+                    {this.props.cart.length === 0 && <div className="empty-cart-container"><h4>Not much here...</h4> <Button bsSize="xsmall" href="/products">Continue Shopping</Button></div>}
+                    {this.props.cart.length > 0 && <CheckoutForm />}
                 </Row>
             </Grid>
         )
