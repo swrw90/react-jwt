8a6fd638b9c94ea0a942fa444e566b13e54af8d7
Seth Watson <seth.watson1@gmail.com>
2018-04-25 23:04:42 -0700

    more styling

--- client/src/Login/Login.jsx
+++ client/src/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Col, ControlLabel, Button, Checkbox, Form, FormGroup, FormControl } from 'react-bootstrap';
+import { Grid, Col, Image, Jumbotron, ControlLabel, Button, Checkbox, Form, FormGroup, FormControl } from 'react-bootstrap';
 import './Login.css';
 import { connect } from 'react-redux';
 import { login } from '../redux/reducers/login.reducers';
@@ -16,39 +16,36 @@ class Login extends Component {
         let { isLoginPending, isLoginSuccess, loginError } = this.props
         return (
             <div>
-                <Grid className="login-form-wrapper" onSubmit={this.onSubmit}>
-                    <Form horizontal name="loginForm">
-                        <FormGroup controlId="formHorizontalEmail">
-                            <Col componentClass={ControlLabel} sm={2}>
-                                Email
-                            </Col>
-                            <Col sm={10}>
-                                <FormControl type="email" name="email" placeholder="Email" onChange={e => this.setState({ email: e.target.value })} />
-                            </Col>
-                        </FormGroup>
-                        <FormGroup controlId="formHorizontalPassword">
-                            <Col componentClass={ControlLabel} sm={2}>
-                                Password
-                             </Col>
-                            <Col sm={10}>
-                                <FormControl type="password" name="password" placeholder="Password" onChange={e => this.setState({ password: e.target.value })} />
-                            </Col>
-                        </FormGroup>
-                        <FormGroup>
-                            <Col smOffset={2} sm={10}>
-                                <Checkbox>Remember me</Checkbox>
-                            </Col>
-                        </FormGroup>
-                        <FormGroup>
-                            <Col smOffset={2} sm={10}>
-                                <Button type="submit" value="Login">Sign in</Button>
-                                {isLoginPending && <div>Please wait...</div>}
-                                {isLoginSuccess && <div>Welcome back!</div>}
-                                {loginError && <div>{loginError.message}</div>}
-                            </Col>
-                        </FormGroup>
-                    </Form>
 
+                <Grid className="login-form-wrapper" onSubmit={this.onSubmit}>
+                    <Jumbotron className="cart-banner">
+                        <h1>Clothing Co.</h1>
+                        <p className="cart-banner-text">Sign up for great deals!</p>
+                    </Jumbotron>
+                    <Col sm={10} md={6}>
+                        <Image src="assets/pexels-photo-322207.jpeg" className="about-profile-picture" rounded />
+                    </Col>
+                    <Col sm={10} md={6}>
+                        <Form horizontal name="loginForm">
+                            <FormGroup controlId="formHorizontalEmail">
+                                <Col componentClass={ControlLabel} sm={2} md={12}>
+                                    <FormControl type="email" name="email" placeholder="Email" onChange={e => this.setState({ email: e.target.value })} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup controlId="formHorizontalPassword">
+                                <Col componentClass={ControlLabel} sm={2} md={12}>
+                                    <FormControl type="password" name="password" placeholder="Password" onChange={e => this.setState({ password: e.target.value })} />
+                                    <Checkbox>Remember me</Checkbox>
+                                    <Button type="submit" value="Login">Sign in</Button>
+                                    <Col smOffset={2} sm={10} md={12}>
+                                        {isLoginPending && <div>Please wait...</div>}
+                                        {isLoginSuccess && <div><h3>Welcome back!</h3></div>}
+                                        {loginError && <div>{loginError.message}</div>}
+                                    </Col>
+                                </Col>
+                            </FormGroup>
+                        </Form>
+                    </Col>
                 </Grid>
             </div>
         );
