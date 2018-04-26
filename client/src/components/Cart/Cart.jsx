2027612ed2cb070cb495c3c0843dc517c4847db8
Seth Watson <seth.watson1@gmail.com>
2018-04-25 22:15:25 -0700

    added styling

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
-import { Grid, Row, Col, PageHeader, Thumbnail, Image, Button, Form, Media } from 'react-bootstrap';
+import { Grid, Row, Col, Jumbotron, PageHeader, Thumbnail, Image, Button, Form, Media } from 'react-bootstrap';
 import './cart.css';
 
 class Cart extends React.Component {
     render() {
         return (
             <Grid>
-                <PageHeader>Cart Component</PageHeader>
+                <Jumbotron className="cart-banner">
+                    <h1>Checkout</h1>
+                    <p className="cart-banner-text">We value our customers</p>
+                </Jumbotron>
                 <Row className="show-grid">
                     <Col xs={12} md={8}>
                         <div className="item-container">
                             <Media>
                                 <Media.Left>
-                                    <img width={64} height={64} src='assets/night-product-watch-dramatic-84475.jpeg' alt="thumbnail" />
+                                    <img width={100} height={100} src='assets/night-product-watch-dramatic-84475.jpeg' alt="thumbnail" />
+                                </Media.Left>
+                                <Media.Body>
+                                    <div>
+                                        <Media.Heading>Classy Watch</Media.Heading>
+                                    </div>
+                                    <div>
+                                        <p>Some content about content in the content you purchased content. This items is an item in the cart of items that you may purchase.</p>
+                                        <div>
+                                            <Button bsSize="xsmall">Remove</Button>
+                                            <Button bsSize="xsmall">Edit</Button>
+                                        </div>
+                                    </div>
+                                </Media.Body>
+                            </Media>
+                        </div>
+
+                        <div className="item-container">
+                            <Media>
+                                <Media.Left>
+                                    <img width={100} height={100} src='assets/night-product-watch-dramatic-84475.jpeg' alt="thumbnail" />
                                 </Media.Left>
                                 <Media.Body>
                                     <div>
@@ -32,27 +55,7 @@ class Cart extends React.Component {
                         <div className="item-container">
                             <Media>
                                 <Media.Left>
-                                    <img width={64} height={64} src='assets/night-product-watch-dramatic-84475.jpeg' alt="thumbnail" />
-                                </Media.Left>
-                                <Media.Body>
-                                    <div>
-                                        <Media.Heading>Classy Watch</Media.Heading>
-                                    </div>
-                                    <div>
-                                        <p>Some content about content in the content you purchased content.</p>
-                                        <div>
-                                            <Button bsSize="xsmall">Remove</Button>
-                                            <Button bsSize="xsmall">Edit</Button>
-                                        </div>
-                                    </div>
-                                </Media.Body>
-                            </Media>
-                        </div>
-
-                        <div className="item-container">
-                            <Media>
-                                <Media.Left>
-                                    <img width={64} height={64} src='assets/night-product-watch-dramatic-84475.jpeg' alt="thumbnail" />
+                                    <img width={100} height={100} src='assets/night-product-watch-dramatic-84475.jpeg' alt="thumbnail" />
                                 </Media.Left>
                                 <Media.Body>
                                     <div>
