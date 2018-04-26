57826c65fe3c23542cbc6887e67b5db0cb628130
Seth Watson <seth.watson1@gmail.com>
2018-04-25 23:44:59 -0700

    beginning cart reducer

--- client/src/redux/reducers/cart.reducer.js
+++ client/src/redux/reducers/cart.reducer.js
@@ -1,5 +1,23 @@
-export const LoadingState = {
-    pending: 0,
-    loading: 1,
-    finished: 2
+let defaultState = {
+    cart: []
 };
+
+let cartReducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case "REMOVE_ITEM":
+            //splice item
+            return {
+                ...state,
+                todoItems: newTodoItems
+            };
+        case "UPDATE_ITEM":
+            //Edit Quantity
+            return {
+                ...state,
+            };
+        default:
+            return state;
+    }
+}
+
+export default cartReducer;
